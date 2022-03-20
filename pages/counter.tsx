6c398c4cb6356829abe6d4ebe6f041faef205490
sgdg <seungdeng17@gmail.com>
2022-03-20 20:52:27 +0900

    feat: 카운터 액션 dispatch

--- pages/counter.tsx
+++ pages/counter.tsx
@@ -1,14 +1,39 @@
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as counterActions from '../store/modules/counter';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
 const Counter: NextPage = () => {
+  const dispatch = useDispatch();
+
+  const value = useSelector(({ counter }) => counter.value);
+
+  const plus = useCallback(
+    ({ value }) => {
+      dispatch(counterActions.increment({ value }));
+    },
+    [dispatch]
+  );
+
+  const minus = useCallback(
+    ({ value }) => {
+      dispatch(counterActions.decrement({ value }));
+    },
+    [dispatch]
+  );
+
   return (
     <div>
       <Head>
         <title>Create Next App - Counter</title>
       </Head>
 
-      <div>COUNTER!</div>
+      <div>
+        <h1>Counter</h1>
+        <button onClick={() => minus({ value })}>-</button> <span>{value}</span>
+        <button onClick={() => plus({ value })}>+</button>
+      </div>
     </div>
   );
 };
